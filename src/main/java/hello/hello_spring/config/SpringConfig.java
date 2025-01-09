fbc995a44bbfd7f72d79c6cbad5c9168c34061e4
yoonsu98 <yoonsu98@kakao.com>
2025-01-09 22:11:16 +0900

    - DB 연동

--- src/main/java/hello/hello_spring/config/SpringConfig.java
+++ src/main/java/hello/hello_spring/config/SpringConfig.java
@@ -1,10 +1,14 @@
 package hello.hello_spring.config;
 
+import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * packageName    : hello.hello_spring.config
@@ -16,6 +20,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -23,6 +34,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
